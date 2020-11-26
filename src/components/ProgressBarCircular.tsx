7871df2396b643f2992e83b4b749c0c728d51069
Pierre DAILY <contact@pierredaily.com>
2020-11-26 23:17:29 +0800

    ADD - text color animation & size update

--- src/components/ProgressBarCircular.tsx
+++ src/components/ProgressBarCircular.tsx
@@ -8,14 +8,25 @@ const animateCircle = (progress: number) => keyframes`
   }
   30% {
     opacity: 0.3;
-    stroke-dashoffset: ${440 - ((440 / 100) * progress * 0.2)};
+    stroke-dashoffset: ${440 - (440 / 100) * progress * 0.2};
   }
   80% {
     opacity: 0.8;
-    stroke-dashoffset: ${440 - ((440 / 100) * progress * 0.4)};
+    stroke-dashoffset: ${440 - (440 / 100) * progress * 0.4};
   }
   100% {
     opacity: 1;
+  }
+`;
+
+const textAnimation = (color1: string = "black", color2: string = "black") => keyframes`
+  from {
+    fill: ${color1};
+    opacity: 0.4;
+  }
+
+  to {
+    fill: ${color2};
   }
 `;
 
@@ -26,61 +37,82 @@ const Circle = styled.circle`
 `;
 
 interface Circle1Props {
-  readonly colour?: string;
-};
+  readonly color?: string;
+}
 
 const Circle1 = styled(Circle) <Circle1Props>`
-  stroke: ${props => props.colour || "red"};
+  stroke: ${(props) => props.color || "red"};
   stroke-dashoffset: 0;
 `;
 
 interface Circle2Props {
-  readonly colour?: string;
+  readonly color?: string;
   readonly progress: number;
-};
+}
 const Circle2 = styled(Circle) <Circle2Props>`
-  animation: ${(props) => css`${animateCircle(props.progress)}`} 2s linear;
+  animation: ${(props) =>
+    css`
+        ${animateCircle(props.progress)}
+      `}
+    2s linear;
 
-  stroke: ${props => props.colour || "blue"};
+  stroke: ${(props) => props.color || "blue"};
   stroke-dashoffset: calc(
     var(--progress-bar-stroke-offset) -
-      ((var(--progress-bar-stroke-offset) / 100) * ${props => props.progress})
+      ((var(--progress-bar-stroke-offset) / 100) * ${(props) => props.progress})
   );
   transform-origin: center;
   transform: rotate(-90deg);
 `;
 
-const Container = styled.div`
+interface SVGProps {
+  readonly color1?: string;
+  readonly color2?: string;
+}
+
+const SVG = styled.svg <SVGProps>`
+  // transform: rotate(-90deg);
   --progress-bar-stroke-offset: 440;
   --stroke-width: 10px;
   position: relative;
   width: 50%;
 
   text {
-    font-size: 3rem;
+    animation: ${(props) =>
+    css`
+          ${textAnimation(props.color1, props.color2)}
+        `} 2s linear;
+    font-size: 200%;
+    transform-origin: center;
   }
 `;
 
-const SVG = styled.svg`
-  // transform: rotate(-90deg);
-`;
-
 interface Props {
-  colour1?: string,
-  colour2?: string,
-  progress: number
-};
+  color1?: string;
+  color2?: string;
+  progress: number;
+}
 
-const ProgressBarCircular: React.FC<Props> = ({ colour1, colour2, progress }) => {
+const ProgressBarCircular: React.FC<Props> = ({
+  color1,
+  color2,
+  progress,
+}) => {
   const filteredProgress = Math.abs(progress) > 100 ? 100 : Math.abs(progress);
   return (
-    <Container>
-      <SVG viewBox="0 0 150 150">
-        <Circle1 cx="75" cy="75" r="70" colour={colour1}></Circle1>
-        <Circle2 cx="75" cy="75" r="70" colour={colour2} progress={filteredProgress}></Circle2>
-        <text x="50%" y="55%" text-anchor="middle">{filteredProgress}</text>
-      </SVG >
-    </Container>
+    <SVG viewBox="0 0 150 150" color1={color1} color2={color2}>
+      <Circle1 cx="75" cy="75" r="70" color={color1}></Circle1>
+      <Circle2
+        cx="75"
+        cy="75"
+        r="70"
+        color={color2}
+        progress={filteredProgress}
+      ></Circle2>
+      <text x="50%" y="55%" text-anchor="middle" fill={color2}>
+        {filteredProgress}%
+      </text>
+    </SVG>
   );
 };
 
