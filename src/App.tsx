6efea31d0d0332f427d7430d9b7fde6c45955c29
Pierre DAILY <contact@pierredaily.com>
2020-12-01 10:08:36 +0800

    ADD - SVG is centered in the exemple page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { ProgressBarCircular } from './components/ProgressBarCircular';
 
 function App() {
   return (
-    <div className="App">
-      <ProgressBarCircular color1="#5770ff" color2="#c644fd" progress={78} />
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <div className="App" style={{ width: "50%" }}>
+        <ProgressBarCircular color1="#5770ff" color2="#c644fd" progress={78} />
+      </div>
     </div>
   );
 }
